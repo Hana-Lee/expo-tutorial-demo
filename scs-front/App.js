35e5f475f4523af7487ef2056f0e36a31cf81cbb
Hana <i@leehana.co.kr>
2024-01-05 17:52:28 +0900

    * 이미지 저장 기능 추가.

--- scs-front/App.js
+++ scs-front/App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar'
 import { Alert, StyleSheet, View } from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import ImageViewer from './src/components/ImageViewer'
 import Button from './src/components/Button'
 import IconButton from './src/components/IconButton'
@@ -10,14 +10,22 @@ import EmojiPicker from './src/components/EmojiPicker'
 import EmojiList from './src/components/EmojiList'
 import EmojiSticker from './src/components/EmojiSticker'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import * as MediaLibrary from 'expo-media-library'
+import { captureRef } from 'react-native-view-shot'
 
 const PlaceHolderImageSource = require('./src/assets/images/background-image.png')
 
 export default function App() {
+  const imageRef = useRef()
+  const [status, requestPermission] = MediaLibrary.usePermissions()
   const [pickedEmoji, setPickedEmoji] = useState(null)
   const [selectedImage, setSelectedImage] = useState(null)
   const [isModalVisible, setIsModalVisible] = useState(false)
   const [showAppOptions, setShowAppOptions] = useState(false)
+
+  if (status === null) {
+    requestPermission()
+  }
 
   /**
  * 이미지 결과를 처리하는 함수입니다.
@@ -54,35 +62,48 @@ export default function App() {
     setIsModalVisible(false)
   }
 
-  const onSaveImageAsync = () => {
+  const onSaveImageAsync = async () => {
+    try {
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1,
+      })
+
+      await MediaLibrary.saveToLibraryAsync(localUri)
+      if (localUri) {
+        Alert.alert("저장되었습니다.")
+      }
+    } catch (e) {
+      console.error(e)
+    }
   }
 
   return (
     <GestureHandlerRootView style={styles.container}>
-      <View style={styles.container}>
-        <View style={styles.imageContainer}>
+      <View style={styles.imageContainer}>
+        <View ref={imageRef} collapsable={false}>
           <ImageViewer placeHolderImageSource={PlaceHolderImageSource} selectedImage={selectedImage}/>
           {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji}/> : null}
         </View>
-        {showAppOptions ? (
-          <View style={styles.optionsContainer}>
-            <View style={styles.optionsRow}>
-              <IconButton icon={"refresh"} label={"다시 선택"} onPress={onReset}/>
-              <CircleButton onPress={onAddSticker}/>
-              <IconButton icon={"save-alt"} label={"저장"} onPress={onSaveImageAsync}/>
-            </View>
-          </View>
-        ): (
-          <View style={styles.footerContainer}>
-            <Button theme={"primary"} label={"사진 선택"} /* @info */ onPress={pickImageAsync} /* @end *//>
-            <Button label={"사진 사용"} onPress={() => setShowAppOptions(true)}/>
-          </View>
-        )}
-        <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-          <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose}/>
-        </EmojiPicker>
-        <StatusBar style="auto"/>
       </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon={"refresh"} label={"다시 선택"} onPress={onReset}/>
+            <CircleButton onPress={onAddSticker}/>
+            <IconButton icon={"save-alt"} label={"저장"} onPress={onSaveImageAsync}/>
+          </View>
+        </View>
+      ): (
+        <View style={styles.footerContainer}>
+          <Button theme={"primary"} label={"사진 선택"} /* @info */ onPress={pickImageAsync} /* @end *//>
+          <Button label={"사진 사용"} onPress={() => setShowAppOptions(true)}/>
+        </View>
+      )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose}/>
+      </EmojiPicker>
+      <StatusBar style="auto"/>
     </GestureHandlerRootView>
   )
 }
